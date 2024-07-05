8a59c09369e8126593cffda8b161a9df5c93ff73
Paulo Kinjo <paulokinjo@hotmail.com>
2024-07-05 16:36:28 -0400

    Making a bar chart reader

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+public class BarChart
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1, }; // distribution
+        Console.WriteLine("Grade Distribution:");
+
+        // for each array element, output a bar of the chart
+        for (int counter = 0; counter < array.Length; ++counter)
+        {
+            // output bar labels ("00-09: ", ..., "90-99: ", "100: ")
+            if (counter == 10)
+                Console.Write(" 100: ");
+            else
+                Console.Write("{0:D2}-{1:D2}: ",
+            counter * 10, counter * 10 + 9);
+
+            // Display bar of asterisks
+            for (int stars = 0; stars < array[ counter ]; ++stars)
+                Console.Write("*");
+
+            Console.WriteLine(); // start a new line of output
+        } // end outer for
+    } // end Main
+} // end class BarChart
